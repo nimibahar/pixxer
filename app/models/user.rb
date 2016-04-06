61f596df1931a9f451097d954eaef81afaca330d
Nimrod Bahar <nimrodbahar@gmail.com>
2016-04-06 17:04:38 +0200

    try to see user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,13 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :omniauthable, omniauth_providers: [:facebook]
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   def self.find_for_facebook_oauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      p user
       user.provider = auth.provider
       user.uid = auth.uid
       user.email = auth.info.email
