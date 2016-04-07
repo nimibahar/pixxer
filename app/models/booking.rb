6f31f7becde8ace91db890c55612a0b923e17411
Prayag Barua <pbarua@pobox.com>
2016-04-07 16:32:03 +0200

    misc

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,10 @@
 class Booking < ActiveRecord::Base
   belongs_to :user
   belongs_to :table
+
+  def new_from_table(table)
+    self.status = "Hosted"
+    self.price = self.seats * table.per_seat
+    self.table_id = table.id
+  end
 end
