70bed40398ee72239f4a0364815b7bfd2f4ba7ce
Prayag Barua <pbarua@pobox.com>
2016-04-10 16:50:28 +0200

    booking photo

--- app/assets/javascripts/tables.js
+++ app/assets/javascripts/tables.js
@@ -1,8 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $('.tabsts').each(function(){
-   $("td:contains('Hosted')").css('background-color','yellow');
-   $("td:contains('Full')").css('background-color','green');
+   $("tr:contains('Hosted')").css('background-color','#ffff99');
+   $("tr:contains('Full')").css('background-color','#ccffcc');
   // if($('.tabsts').text() == 'Hosted'){
   //   console.log("reached")
   //   $(this).
